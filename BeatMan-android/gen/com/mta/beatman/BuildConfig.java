ea9f847e6015d500d169e4307ccade222162c308
marinbogdan <marin_didina@yahoo.com>
2015-01-12 12:31:51 +0200

    ...

--- BeatMan-android/gen/com/mta/beatman/BuildConfig.java
+++ BeatMan-android/gen/com/mta/beatman/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.youtube.invaders;
+package gen.com.mta.beatman;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;

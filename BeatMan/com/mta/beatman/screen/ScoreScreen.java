e148bdaec7ed61c508613e7866fbefae44d9c97e
Sorin Dunareanu <sorinduni@gmail.com>
2015-01-25 16:38:35 +0200

    score screen

---
+++ BeatMan/com/mta/beatman/screen/ScoreScreen.java
@@ -0,0 +1,57 @@
+package com.youtube.invaders.screen;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.youtube.invaders.MainGame;
+import com.youtube.invaders.TextureManager;
+import com.youtube.invaders.camera.OrthoCamera;
+
+public class ScoreScreen extends Screen {
+
+	private OrthoCamera camera;
+	private Texture texture;
+	
+	public ScoreScreen() {
+	
+	}
+	
+	@Override
+	public void create() {
+		camera = new OrthoCamera();
+		camera.resize();
+	}
+
+	@Override
+	public void update() {
+		camera.update();
+	}
+
+	@Override
+	public void render(SpriteBatch sb) {
+		sb.setProjectionMatrix(camera.combined);
+		sb.begin();
+		//sb.draw(texture, MainGame.WIDTH / 2 - texture.getWidth() / 2, MainGame.HEIGHT / 2 - texture.getHeight() / 2);
+		sb.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		camera.resize();
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+}
